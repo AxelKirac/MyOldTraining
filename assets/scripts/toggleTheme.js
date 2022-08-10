46467c5ac7a51044e5f6fcd5462b35a0b54b95e1
Ikikaeru <39247240+Ikikaeru@users.noreply.github.com>
2022-08-10 10:13:40 +0200

    First commit - Project base

---
+++ assets/scripts/toggleTheme.js
@@ -0,0 +1,12 @@
+
+let dark = false;
+const switchTheme = document.querySelector('.theme_switch');
+switchTheme.addEventListener('click', () => {
+    dark = !dark;
+    const elemsDark = document.querySelectorAll(dark ? '.light' : '.dark');
+    for(const e of elemsDark)
+    {
+        e.classList.add(dark ? 'dark' : 'light');
+        e.classList.remove(dark ? 'light' : 'dark');
+    }
+})
