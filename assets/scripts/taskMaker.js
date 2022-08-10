c859f56a32dff833ea89946a674712b165be4f74
Ikikaeru <39247240+Ikikaeru@users.noreply.github.com>
2022-08-10 16:15:01 +0200

    New task HTML + CSS + JS show/hide

--- assets/scripts/taskMaker.js
+++ assets/scripts/taskMaker.js
@@ -1,36 +1,72 @@
-const todoNewTask = document.querySelector('.toDo_newTask');
-const doingNewTask = document.querySelector('.doing_newTask');
+const todoNewTask = document.querySelector('.toDo_newTask > .toDo_newTask_title');
+const doingNewTask = document.querySelector('.doing_newTask > .doing_newTask_title');
 todoNewTask.addEventListener('click', (e) => {
-    let icon = newTaskIcon('todo');
+    toggleIcon('todo');
+});
+doingNewTask.addEventListener('click', (e) => {
+    toggleIcon('doing');
+});
+
+function toggleIcon(event) {
+    let icon = newTaskIcon(event);
     if(icon !== null)
     {
         if(icon.innerHTML === '-')
         {
             icon.innerHTML = '+';
+            toggleDropdown(event, false);
         }
         else
         {
             icon.innerHTML = '-';
+            toggleDropdown(event, true);
         }
     }
-});
-doingNewTask.addEventListener('click', (e) => {
-    //newTaskIcon('doing');
-});
-
-function newTaskIcon(event) {
-    let newTaskIcon;
+}
+function toggleDropdown(event, isVisible) {
+    let drop = newTaskDropdown(event);
+    if(drop !== null)
+    {
+        if(isVisible)
+        {
+            drop.style.display = 'flex';
+        }
+        else
+        {
+            drop.style.display = 'none';
+        }
+    }
+}
+function newTaskDropdown(event) {
+    let newTaskDrp;
     switch(event)
     {
         case 'todo':
-            newTaskIcon = document.querySelector('.toDo_newTask_title > .toDo_newTask_icon');
+            newTaskDrp = document.querySelector('.toDo_newTask > .maketask');
             break;
         case 'doing':
-            newTaskIcon = document.querySelector('.doing_newTask_title > .toDo_newTask_icon');
+            newTaskDrp = document.querySelector('.doing_newTask > .maketask');
             break;
         default:
-            newTaskIcon = null;
+            newTaskDrp = null;
             break;
     }
-    return newTaskIcon;
+    return newTaskDrp;
+
+}
+function newTaskIcon(event) {
+    let newTaskIcn;
+    switch(event)
+    {
+        case 'todo':
+            newTaskIcn = document.querySelector('.toDo_newTask_title > .toDo_newTask_icon');
+            break;
+        case 'doing':
+            newTaskIcn = document.querySelector('.doing_newTask_title > .doing_newTask_icon');
+            break;
+        default:
+            newTaskIcn = null;
+            break;
+    }
+    return newTaskIcn;
 }
