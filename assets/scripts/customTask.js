85ebe9d09361d5a29264384819a641c3603c44ea
Ikikaeru <39247240+Ikikaeru@users.noreply.github.com>
2022-08-11 18:06:59 +0200

    Refactor customTask and dragAndDrop

--- assets/scripts/customTask.js
+++ assets/scripts/customTask.js
@@ -11,45 +11,11 @@ class CustomTask {
     get DueDate() {
         return (this.dueDate - new Date(inputActualDateFormat())) / 86400000;
     }
+    get Category() {
+        return this.category === 'todo' ? 'toDo' : this.category;
+    }
     setCategory(newCat)
     {
         this.category = newCat;
-    }
-}
-
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.getAttribute('value'));
-}
-  
-function drop(ev) {
-    ev.preventDefault();
-    let data = ev.dataTransfer.getData("text");
-    let TARGET = ev.target;
-    let CONTENT = ev.target.getAttribute('content');
-    if(!CONTENT)
-    {
-        CONTENT = ev.target.closest('.task').parentNode.getAttribute('content');
-        TARGET = document.querySelector(`.${CONTENT === 'todo' ? 'toDo' : CONTENT}_tasks`);
-    }
-    data = Number(data);
-    let category;
-    for(let i = 0; i < userTasks.length; i++)
-    {
-        if(userTasks[i].id == data)
-        {
-            category = userTasks[i].category;
-            userTasks[i].setCategory(CONTENT);
-            break;
-        }
-    }
-    if(category !== undefined && category !== null)
-    {
-        category = category === 'todo' ? 'toDo' : category;
-        TARGET.appendChild(document.querySelector(`.${category}_tasks > .task[value='${data}']`));
-        saveDatas();
     }
 }
