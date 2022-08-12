b64019735cb6f660defb2cda55dbd6e43e8efc22
FriquetLuca <friquet.luca@gmail.com>
2022-08-12 12:03:41 +0200

    Eye checkboxes

--- assets/scripts/filter.js
+++ assets/scripts/filter.js
@@ -1,21 +1,29 @@
 class Filter {
-    constructor(button, task)
+    constructor(button, img, task)
     {
         this.button = button;
+        this.img = img;
         this.task = task;
     }
 }
 const FILTERS = [
-    new Filter('.dropdown-content-todo > .dropdown-content-checkbox', '.toDo'),
-    new Filter('.dropdown-content-doing > .dropdown-content-checkbox', '.doing'),
-    new Filter('.dropdown-content-done > .dropdown-content-checkbox', '.done')
+    new Filter('.dropdown-content-todo', '.dropdown-content-checkbox', '.toDo'),
+    new Filter('.dropdown-content-doing', '.dropdown-content-checkbox', '.doing'),
+    new Filter('.dropdown-content-done', '.dropdown-content-checkbox', '.done')
 ];
 for(const filter of FILTERS)
 {
-    let btn = document.querySelector(filter.button)
+    let btn = document.querySelector(filter.button);
+    let img = document.querySelector(`${filter.button} > ${filter.img}`);
     let task = document.querySelector(filter.task);
     btn.addEventListener("click", () => {
-        if(btn.checked) {
+        let actualValue = img.getAttribute('value') === 'true' ? 'false' : 'true';
+        img.setAttribute('value', actualValue);
+        let darkAppend = dark ? '-dark' : '';
+        let imgPath = `assets/img/${actualValue === 'true' ? 'eye-icon' : 'closed-eye-icon'}${darkAppend}.png`;
+        img.setAttribute('src', imgPath);
+        img.setAttribute('alt', actualValue === 'true' ? 'X' : 'V');
+        if(actualValue === 'true') {
             task.style.display = 'block';
         }
         else {
--- assets/scripts/toggleTheme.js
+++ assets/scripts/toggleTheme.js
@@ -9,4 +9,16 @@ switchTheme.addEventListener('click', () => {
         e.classList.add(dark ? 'dark' : 'light');
         e.classList.remove(dark ? 'light' : 'dark');
     }
+    const checkboxesMode = [
+        '.dropdown-content-todo > .dropdown-content-checkbox',
+        '.dropdown-content-doing > .dropdown-content-checkbox',
+        '.dropdown-content-done > .dropdown-content-checkbox'
+    ];
+    let darkAppend = dark ? '-dark' : '';
+    for(const md of checkboxesMode)
+    {
+        let img = document.querySelector(md);
+        let imgPath = `assets/img/${img.getAttribute('value') === 'true' ? 'eye-icon' : 'closed-eye-icon'}${darkAppend}.png`;
+        img.setAttribute('src', imgPath);
+    }
 });
