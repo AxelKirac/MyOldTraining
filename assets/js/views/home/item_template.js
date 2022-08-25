f16fa661b73aa86d6b2553ff9263c3b875f1467b
AxelKirac <axel.kirac2000@gmail.com>
2022-08-25 15:34:12 +0200

    modif home and add item_template.js

---
+++ assets/js/views/home/item_template.js
@@ -0,0 +1,29 @@
+export function TemplateItems(name) {
+    
+    
+    
+    return ` <li class="event__list__items">
+    
+    <p>${name}</p>
+    
+    <div class="event__list__items__options">
+        
+        <button id="event__list__items__options__delete__id" class="event__list__items__options__delete">
+            <img src="assets/img/delete_black.png" alt="delete" class="event__list__items__options__delete__img">
+        </button>
+        
+        <button id="event__list__items__options__delete__id" class="event__list__items__options__edit">
+            <img src="assets/img/edit_black.png" alt="edit" class="event__list__items__options__edit__img">
+        </button>
+        
+        <button id="event__list__items__options__delete__id" class="event__list__items__options__join">
+            <img src="assets/img/join_black.png" alt="join" class="event__list__items__options__join__img">
+        
+        </button>
+    
+    </div>
+
+</li>
+</ul>`
+
+}
