44bdbdcc1a5de86844d96c90cc94050243c0152c
axel kirac <axel.kirac2000@gmail.com>
2022-08-26 11:31:34 +0200

    fix display_Items

--- assets/js/views/home/displays_items.js
+++ assets/js/views/home/displays_items.js
@@ -5,13 +5,11 @@ import { FetchApi } from "../../controllers/API/fetchApi";
 
 export async function DisplayEvenlist(){
     const NewApi = new FetchApi()
-    const EventName = NewApi.getEventList();
-    console.log(EventName)
-    EventName.forEach((item) => {
+    const EventName = await NewApi.getEventList();
+    EventName.forEach(item => {
         const template = TemplateItems(item.name);
         const element = templateElement(template);
         const main = document.querySelector('#event--list');
         main.appendChild(element);
     });
 }
-// EventName.forEach((items) => TemplateItems(items.name));
