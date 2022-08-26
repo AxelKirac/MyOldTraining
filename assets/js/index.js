6eebd6fef2fb8ead35ee9b046394bee48252ce5d
GuillaumeB <guibau2303@gmail.com>
2022-08-26 14:37:03 +0200

    refactor build event js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ import { showEvent } from "./views/event/show-event.js";
 import { FetchApi } from "./controllers/API/fetchApi.js"
 import { DisplayEvenlist } from "./views/home/displays_items.js";
 
+
 // const method = 'POST'
 
 // const data = { name: 'super event', dates: ['2023-10-24', '2023-10-25', '2023-10-26'], author: 'toto', description: 'test event' }
