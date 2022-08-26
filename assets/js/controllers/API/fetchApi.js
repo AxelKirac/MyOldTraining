c7eddd9e694442535032e16a82c0192c3237cd39
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-08-26 08:57:42 +0200

    Add doc to fetchApi.js

--- assets/js/controllers/API/fetchApi.js
+++ assets/js/controllers/API/fetchApi.js
@@ -1,7 +1,18 @@
 import { SETTINGS } from "../../models/settings.js"
 
+/**
+ * Class to manage API requests.
+ */
 export class FetchApi {
 
+    /**
+     * Make an API request with provided datas.
+     * 
+     * @param {String} route 
+     * @param {String} method 
+     * @param {Object} body 
+     * @returns {Object}
+     */
     async fetchApi(route, method, body) {
         const datas = {
             method: method,
@@ -24,51 +35,164 @@ export class FetchApi {
         return output
     }
 
+    /**
+     * Get a list of all the events.
+     * 
+     * @returns {Object}
+     */
     getEventList() {
         const route = "/api/events/"
         return this.fetchApi(route, 'GET', null)
     }
 
+    /**
+     * Get the event with the provided id.
+     * 
+     * @param {String} id
+     * @returns {Object}
+     */
     getEvent(id) {
         const route = `/api/events/${id}`
         return this.fetchApi(route, 'GET', null)
     }
 
+    /**
+     * Get a list of all the attendees, and the events they're attending.
+     * 
+     * @returns {Object}
+     */
     getAttendees() {
         const route = `/api/attendees/`
         return this.fetchApi(route, 'GET', null)
     }
 
+    /**
+     * Get all attendances for a given name.
+     * 
+     * @param {String} name
+     * @returns { Object}
+     */
     getAttendances(name) {
         const route = `/api/attendees/${name}`
         return this.fetchApi(route, 'GET', null)
     }
 
+    /**
+     * Creates an event with dates as possibilities.
+     * You must provide an author, a name and a description for the event
+     * 
+     * @param {Object} data - {
+     *                          name: string,
+     *                          dates: [
+     *                            ['YYYY-MM-DD']
+     *                            ['YYYY-MM-DD']
+     *                            ['YYYY-MM-DD']
+     *                          ],
+     *                          author: string,
+     *                          description: string
+     *                        }
+     * 
+     * @returns {Object}
+     */
     createEvent(data) {
         const route = `/api/events/`
         return this.fetchApi(route, 'POST', data)
     }
 
+    /**
+     * Patches(edit) an event with the provided infos.
+     * 
+     * @param {String} id
+     * @param {Object} data - {
+     *                          name: string(optional),
+     *                          author: string(optional),
+     *                          description: string(optional)
+     *                        }
+     * @returns {Object}
+     */
     updateEvent(id, data) {
         const route = `/api/events/${id}`
         return this.fetchApi(route, 'PATCH', data)
     }
 
+    /**
+     * Deletes the event with provided id.
+     * 
+     * @param {String} id
+     * @returns {Object}
+     */
     deleteEvent(id) {
         const route = `/api/events/${id}`
         return this.fetchApi(route, 'DELETE', null)
     }
 
+    /**
+     * Add some possible dates to an event.
+     * 
+     * @param {String} id
+     * @param {Object} data - {
+     *                          dates: [
+     *                            ['YYYY-MM-DD']
+     *                            ['YYYY-MM-DD']
+     *                            ['YYYY-MM-DD']
+     *                          ]
+     *                        }
+     * @returns { Object}
+     */
     addPossibleDates(id, data) {
         const route = `/api/events/${id}/add_dates`
         return this.fetchApi(route, 'POST', data)
     }
 
+    /**
+     * Add an attendance for the given event.
+     * You must provide the attendee's name and some availabilities,
+     * in the form of an array of object
+     * 
+     * @param {String} id
+     * @param {Object} data - {
+     *                          name: string,
+     *                              dates : [
+     *                                  { date: ['YYYY-MM-DD'],
+     *                                    available: boolean(true / false)
+     *                                  }
+     *                                  { date: ['YYYY-MM-DD'],
+     *                                    available: boolean(true / false)
+     *                                  }
+     *                                  { date: ['YYYY-MM-DD'],
+     *                                    available: boolean(true / false)
+     *                                  }
+     *                              ]
+     *                         }
+     * @returns { Object}
+     */
     addAttendences(id, data) {
         const route = `/api/events/${id}/attend`
         return this.fetchApi(route, 'POST', data)
     }
 
+    /**
+     * Edit an attendance for the given event.
+     * You must provide the attendee's name and some availabilities,
+     * in the form of an array of object.
+     * 
+     * @param {String} id
+     * @param {Object} data - {
+     *                          name: string,
+     *                              dates : [
+     *                                  { date: ['YYYY-MM-DD'],
+     *                                    available: boolean(true / false)
+     *                                  }
+     *                                  { date: ['YYYY-MM-DD'],
+     *                                    available: boolean(true / false)
+     *                                  }
+     *                                  { date: ['YYYY-MM-DD'],
+     *                                    available: boolean(true / false)
+     *                                  }
+     *                              ]
+     *                         }
+     * @returns { Object}
+     */
     editAttendences(id, data) {
         const route = `/api/events/${id}/attend`
         return this.fetchApi(route, 'PATCH', data)
