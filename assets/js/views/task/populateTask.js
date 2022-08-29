3cd83b3b39d82646fd323641ebd4a20a6e2ca7e0
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-08-29 16:02:06 +0200

    put populate task in new file

---
+++ assets/js/views/task/populateTask.js
@@ -0,0 +1,25 @@
+export const populateTask = async (id, NewApi) => {
+    const event = await NewApi.getEvent(id)
+
+    updateAuthorInput(event.author)
+    updateEventNameInput(event.name)
+    updateEventDescriptionInput(event.description)
+}
+
+const updateAuthorInput = (author) => {
+    const authorNameInput = document.getElementById('event-author-input')
+
+    authorNameInput.value = author
+}
+
+const updateEventNameInput = (eventName) => {
+    const eventNameInput = document.getElementById('event-name-input')
+
+    eventNameInput.value = eventName
+}
+
+const updateEventDescriptionInput = (eventDescription) => {
+    const eventDescriptionInput = document.getElementById('event-description-input')
+
+    eventDescriptionInput.value = eventDescription
+}
--- assets/js/views/task/showTask.js
+++ assets/js/views/task/showTask.js
@@ -1,5 +1,6 @@
 import { buildTask } from "./buildTask.js"
 import { buildDateInput } from "./buildDateInput.js"
+import { populateTask } from "./populateTask.js"
 
 export const showTask = (id = null, NewApi) => {
     buildTask()
@@ -11,30 +12,4 @@ const initNewTask = () => {
     const firstTask = 1
 
     buildDateInput(firstTask)
-}
-
-const populateTask = async (id, NewApi) => {
-    const event = await NewApi.getEvent(id)
-
-    updateAuthorInput(event.author)
-    updateEventNameInput(event.name)
-    updateEventDescriptionInput(event.description)
-}
-
-const updateAuthorInput = (author) => {
-    const authorNameInput = document.getElementById('event-author-input')
-
-    authorNameInput.value = author
-}
-
-const updateEventNameInput = (eventName) => {
-    const eventNameInput = document.getElementById('event-name-input')
-
-    eventNameInput.value = eventName
-}
-
-const updateEventDescriptionInput = (eventDescription) => {
-    const eventDescriptionInput = document.getElementById('event-description-input')
-
-    eventDescriptionInput.value = eventDescription
 }
