d49fe7797b0cd549e7c942e9be52f795ff842edf
AxelKirac <axel.kirac2000@gmail.com>
2022-08-30 09:28:25 +0200

    start listener button for edit

---
+++ assets/js/controllers/editButtonListeners.js
@@ -0,0 +1,31 @@
+// import { showTask } from "../views/task/showTask.js"
+
+// function getEditButtonId(id) {
+//     return `#event__list__items__options__edit__${id}`
+// }
+
+// export function addEditButtonListener(NewApi, id) {
+
+// }
+
+
+
+
+
+
+
+
+
+
+// function getJoinButtonId(id) {
+//     return `#event__list__items__options__join__${id}`
+// }
+
+// export function addJoinButtonListener(id, NewApi) {
+//     const buttonid = getJoinButtonId(id);
+//     const buttonJoin = document.querySelector(getJoinButtonId(id));
+//     console.log(buttonJoin)
+//     buttonJoin.addEventListener('click', () => showEvent(id, NewApi))
+//     // console.log(buttonid)
+//     // deleteButtonListeners(id, buttonDelete, NewApi);
+// }
