0492079b729a397a81b50b3286fa12d16b1ad60e
AxelKirac <axel.kirac2000@gmail.com>
2022-08-30 14:00:32 +0200

    clean indexjs

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,19 +3,6 @@ import { displayItems } from "./views/home/displays_items.js";
 
 const NewApi = new FetchApi()
 
-const id = '81baeca1afc0'
-const newEvent = {
-    name: 'mega event',
-    dates: [
-        '2022-10-05',
-        '2022-10-06',
-    ],
-    author: 'Jean',
-    description: 'un supermega event'
-}
-
-const newId = "20e8e7d46fbc";
-
 displayItems();
 
 
