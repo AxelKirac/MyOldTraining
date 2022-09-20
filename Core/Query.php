89f3946215f0bc6093a58d6d5e2383a967a53e95
Ikikaeru <39247240+Ikikaeru@users.noreply.github.com>
2022-09-20 16:02:46 +0200

    Comment Query and add kill PDO method

--- Core/Query.php
+++ Core/Query.php
@@ -16,23 +16,38 @@ class Query {
             die('Erreur : '.$e->getMessage());
         }
     }
-    public function fetchAllQuery($query) {
+    /**
+     * Select every value from a query.
+    */
+    public function fetchAll($query) {
         $req = $this->db->prepare($query);
         $req->execute();
         $res = $req->fetchAll();
         $req = null;
         return $res;
     }
-    public function fetchAssocQuery($query) {
+    /**
+     * Select a specific value from a query.
+    */
+    public function fetchAssoc($query) {
         $req = $this->db->prepare($query);
         $req->execute();
         $res = $req->fetch(PDO::FETCH_ASSOC);
         $req = null;
         return $res;
     }
-    public function executeQuery($query, $exec = null) {
+    /**
+     * Execute a query. (Ex: delete, trigger, update, ...).
+     */
+    public function execute($query, $exec = null) {
         $req = $this->db->prepare($query);
         $req->execute($exec) or die(print_r($this->db->errorInfo()));
         $req = null;
+    }
+    /**
+     * Kill the PDO connection.
+     */
+    public function kill() {
+        $this->db = null;
     }
 }
