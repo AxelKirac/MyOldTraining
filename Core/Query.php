8f0434e678c91d648f9aee01b46f0182fc444123
Ikikaeru <39247240+Ikikaeru@users.noreply.github.com>
2022-09-20 15:01:43 +0200

    Add query class

---
+++ Core/Query.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Core;
+
+class Query {
+    private $db;
+    public function __construct($dbName, $host = "localhost", $user = 'root', $pswd = '') {
+        try
+        {
+            // On se connecte à MySQL
+            $this->db = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8", $user, $pswd);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(Exception $e)
+        {
+            die('Erreur : '.$e->getMessage());
+        }
+    }
+    public function fetchAllQuery($query) {
+        $req = $this->db->prepare($query);
+        $req->execute();
+        $res = $req->fetchAll();
+        $req = null;
+        return $res;
+    }
+    public function fetchAssocQuery($query) {
+        $req = $this->db->prepare($query);
+        $req->execute();
+        $res = $req->fetch(PDO::FETCH_ASSOC);
+        $req = null;
+        return $res;
+    }
+    public function executeQuery($query, $exec = null) {
+        $req = $this->db->prepare($query);
+        $req->execute($exec) or die(print_r($this->db->errorInfo()));
+        $req = null;
+    }
+}
--- Resources/views/welcome.php
+++ Resources/views/welcome.php
@@ -10,20 +10,10 @@
 </head>
 <body>
     <main>
-       <section class="container">
-        <img 
-                src="assets/img/Illustration.png" 
-                alt="cogip logo"
-                class="illustration">
+        <section class="container">
+            <img src="assets/img/Illustration.png" alt="cogip logo">
             <h1>Welcome to <?php echo $name ?></h1>
-            <p>This base project is provided by BeCode</p>
-            <ul>
-                <li><a href="https://github.com/bramus/router" target="_blank">Bramus/Router</a></li>
-                <li><a href="https://github.com/filp/whoops" target="_blank">Flip/Whoops</a></li>
-                <li>dd() function</li>
-                <li>redirect() function</li>
-            </ul>
-       </section>
+        </section>
     </main>
 </body>
 </html>
