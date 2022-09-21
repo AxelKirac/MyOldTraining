d5b35b5ac080b40bb0217b7e957b6004a5ae9825
Ikikaeru <39247240+Ikikaeru@users.noreply.github.com>
2022-09-21 16:54:02 +0200

    Remove useless .sql and add triggers for auto-update

--- Database/cogip_db.sql
+++ Database/cogip_db.sql
@@ -1,16 +1,26 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+01:00";
+set SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+set time_zone = "+01:00";
 
-CREATE DATABASE IF NOT EXISTS cogip;
-USE cogip;
+create database if not exists cogip;
+use cogip;
 
 create table if not exists `roles` (
     `id` int unsigned not null AUTO_INCREMENT,
     `name` varchar(50) character set utf8 not null,
     `created_at` datetime default CURRENT_TIMESTAMP,
-    `updated_at` datetime not null,
+    `updated_at` datetime default CURRENT_TIMESTAMP,
     primary key (id)
 ) ENGINE=InnoDB default CHARSET=latin1 AUTO_INCREMENT=1;
+
+delimiter $$
+create trigger roles_update before update on roles
+for each row
+begin
+    if new.name != old.name then
+        set new.updated_at = CURRENT_TIMESTAMP;
+    end if;
+end$$
+delimiter ;
 
 create table if not exists `permissions` (
     `id` int unsigned not null AUTO_INCREMENT,
@@ -19,6 +29,16 @@ create table if not exists `permissions` (
     `updated_at` datetime not null,
     primary key (id)
 ) ENGINE=InnoDB default CHARSET=latin1 AUTO_INCREMENT=1;
+
+delimiter $$
+create trigger permissions_update before update on `permissions`
+for each row
+begin
+    if new.name != old.name then
+        set new.updated_at = CURRENT_TIMESTAMP;
+    end if;
+end$$
+delimiter ;
 
 
 create table if not exists `roles_permission` (
@@ -43,6 +63,20 @@ create table if not exists `users` (
     foreign key (role_id) references roles(id)
 ) ENGINE=InnoDB default CHARSET=latin1 AUTO_INCREMENT=1;
 
+delimiter $$
+create trigger permissions_update before update on users
+for each row
+begin
+    if      new.first_name != old.first_name
+        or  new.last_name != old.last_name
+        or  new.email != old.email
+        or  new.password != old.password
+        or  new.role_id != old.role_id then
+            set new.updated_at = CURRENT_TIMESTAMP;
+    end if;
+end$$
+delimiter ;
+
 create table if not exists `types` (
     `id` int unsigned not null AUTO_INCREMENT,
     `name` varchar(50) character set utf8 not null,
@@ -50,6 +84,16 @@ create table if not exists `types` (
     `updated_at` datetime not null,
     primary key (id)
 ) ENGINE=InnoDB default CHARSET=latin1 AUTO_INCREMENT=1;
+
+delimiter $$
+create trigger permissions_update before update on `types`
+for each row
+begin
+    if new.name != old.name then
+        set new.updated_at = CURRENT_TIMESTAMP;
+    end if;
+end$$
+delimiter ;
 
 create table if not exists `companies` (
     `id` int unsigned not null AUTO_INCREMENT,
@@ -63,6 +107,19 @@ create table if not exists `companies` (
     foreign key (type_id) references types(id)
 ) ENGINE=InnoDB default CHARSET=latin1 AUTO_INCREMENT=1;
 
+delimiter $$
+create trigger permissions_update before update on companies
+for each row
+begin
+    if      new.type_id != old.type_id
+        or  new.name != old.name
+        or  new.country != old.country
+        or  new.tva != old.tva then
+            set new.updated_at = CURRENT_TIMESTAMP;
+    end if;
+end$$
+delimiter ;
+
 create table if not exists `contacts` (
     `id` int unsigned not null AUTO_INCREMENT,
     `company_id` int unsigned not null,
@@ -74,14 +131,40 @@ create table if not exists `contacts` (
     primary key (id),
     foreign key (company_id) references companies(id)
 ) ENGINE=InnoDB default CHARSET=latin1 AUTO_INCREMENT=1;
+
+delimiter $$
+create trigger permissions_update before update on contacts
+for each row
+begin
+    if      new.company_id != old.company_id
+        or  new.name != old.name
+        or  new.email != old.email
+        or  new.phone != old.phone then
+            set new.updated_at = CURRENT_TIMESTAMP;
+    end if;
+end$$
+delimiter ;
     
 
 create table if not exists `invoices` (
     `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
     `ref` varchar(50) character set utf8 not null,
     `id_company` int unsigned not null,
+    `due_date` datetime not null,
     `created_at` datetime default CURRENT_TIMESTAMP,
     `updated_at` datetime not null,
     primary key (id),
     foreign key (id_company) references companies(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+delimiter $$
+create trigger permissions_update before update on invoices
+for each row
+begin
+    if      new.ref != old.ref
+        or  new.id_company != old.id_company
+        or  new.due_date != old.due_date then
+            set new.updated_at = CURRENT_TIMESTAMP;
+    end if;
+end$$
+delimiter ;
