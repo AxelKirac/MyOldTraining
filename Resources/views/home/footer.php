7bfe29889cc3e4150c1b2ce0ddfa92df02521e6c
Anthony <anthony.denis.becode@gmail.com>
2022-09-22 15:42:23 +0200

    fusion

---
+++ Resources/views/home/footer.php
@@ -0,0 +1,40 @@
+<footer>
+    <section>
+    <div class="separator"></div>
+    </section>
+    
+    <section>
+        <div class="title"><h1>COGIP</h1></div>
+        <div class="Information">
+            <p> <img src="assets/img/Place.svg" alt=""> Square des Martyrs , 6000 Charleroi</p>
+            <div class="number_align">
+                <p> <img src="assets/img/Path.svg" alt=""> (123) 456-789 </p>
+                <p> <img src="assets/img/shape.svg" alt=""> (123) 456-789 </p>
+            </div>
+            <div class="number_align">
+                <p>social Media</p>
+                <img src="assets/img/logo_facebook.svg" alt="">
+                <img src="assets/img/logo_twitter.svg" alt="">
+                <img src="assets/img/logo_linkedin.svg" alt="">
+                <img src="assets/img/logo_youtube.svg" alt="">
+                <img src="assets/img/logo_instagram.svg" alt="">
+                <img src="assets/img/logo_googleplus.svg" alt="">
+                <img src="assets/img/logo_pinterest.svg" alt="">
+                <img src="assets/img/logo_wifi_access.svg" alt="">
+            </div>
+        </div>
+
+    </section>
+
+    <div class="menu">
+        <a href="">HOME</a>
+        <a href="">INVOICES</a>
+        <a href="">COMPANIES</a>
+        <a href="">CONTACTS</a>
+        <a href="">PRIVACY POLICY</a>  
+    </div>
+    <div>
+        <p>Copyright © 2022 • COGIP Inc.</p>
+    </div>
+    
+</footer>
---
+++ Resources/views/invoices/main.php
@@ -0,0 +1,39 @@
+<main>
+    <div>
+        <div class="title">
+            <h1>All invoices</h1>
+        </div>
+
+        <div class="input_search">
+            <input type="text">
+        </div>
+
+        <section class="table_title table_align">
+            <div><p>Invoices number</p></div>
+            <div><p>Dates due</p></div>
+            <div><p>Company</p></div>
+            <div><p>Created at</p></div>
+        </section>
+
+        <div>
+            <section class="table_content table_align">
+            <div><p>FE 85 95 78 98</p></div>
+            <div><p>15/09/2022</p></div>
+            <div><p>Jouet Jean Michel</p></div>
+            <div><p>25/09/2022</p></div>
+            </section>
+        </div>
+
+        <div class="navigateur_pages">
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+        </div>
+
+    </div>
+</main>
