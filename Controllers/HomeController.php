4cbc16a5b33d76041a4d2fed36ea1ff07f9b01e4
FriquetLuca <friquet.luca@gmail.com>
2022-09-22 13:29:10 +0200

    Delete dummy scripts

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -11,6 +11,15 @@ class HomeController extends Controller
     */
     public function index()
     {
-        return $this->view('home',["name" => "Cogip"]);
+        $db = new Query('cogip');
+        $invoicesResult=$db->fetchAll("SELECT ref, created_at FROM invoices ORDER BY created_at DESC LIMIT 5");
+        $contactsResult=$db->fetchAll("SELECT name, phone, email, created_at FROM contacts ORDER BY created_at DESC LIMIT 5");
+        $companiesResult=$db->fetchAll("SELECT name, tva, country, created_at FROM companies ORDER BY created_at DESC LIMIT 5");
+        $result = [
+            "contacts"=>$contactsResult,
+            "invoices"=>$invoicesResult,
+            "companies"=>$companiesResult
+        ];
+        return $this->view('home', $result);
     }
 }
