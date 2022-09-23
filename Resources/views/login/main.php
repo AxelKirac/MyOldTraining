5fc42a7e4cdb5d765fb9eee6e00e01b069dff216
FriquetLuca <friquet.luca@gmail.com>
2022-09-23 09:40:10 +0200

    Login + Signup template view

---
+++ Resources/views/login/main.php
@@ -0,0 +1,5 @@
+<form action="post">
+    <input type="email" name="mail" id="mail" placeholder="Enter your email adress...">
+    <input type="password" name="pswd" id="pswd" placeholder="Enter your password...">
+    <input type="submit" value="Connection">
+</form>
---
+++ Resources/views/signup/main.php
@@ -0,0 +1,8 @@
+<form action="post">
+    <input type="text" name="firstname" id="firstname" placeholder="Enter your first name...">
+    <input type="text" name="lastname" id="lastname" placeholder="Enter your last name...">
+    <input type="email" name="mail" id="mail" placeholder="Enter your email adress...">
+    <input type="password" name="pswd" id="pswd" placeholder="Enter a password...">
+    <input type="password" name="pswdconfirm" id="pswdconfirm" placeholder="Enter the same password again...">
+    <input type="submit" value="Submit">
+</form>
