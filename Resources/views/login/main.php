fc7c6f25ee9626dc457c379fa97ce4a3dccd4194
AxelKirac <axel.kirac2000@gmail.com>
2022-10-07 13:57:10 +0200

    Fix login style

--- Resources/views/login/main.php
+++ Resources/views/login/main.php
@@ -1,4 +1,4 @@
-<form action="" method="" id="logging">
+<form class="signuploginform" action="" method="" id="logging">
     <input type="email" class="email" name="mail" id="mail" placeholder="Enter your email adress...">
     <input type="password" class="password" name="pswd" id="pswd" placeholder="Enter your password...">
     <input type="submit" class="submit" value="Connection">
