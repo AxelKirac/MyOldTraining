3180106158a9177c10f447443f1b2527e5a6fd27
Anthony <anthony.denis.becode@gmail.com>
2022-10-07 14:34:00 +0200

    fix .

--- Resources/views/contacts/main.php
+++ Resources/views/contacts/main.php
@@ -5,7 +5,7 @@
             </div>
 
             <div class="input_search_invoices">
-                <input type="text" class="invoices_input_search" placeholder="Search company">
+                <input type="text" class="invoices_input_search" placeholder="Search contacts">
             </div>
 
         <section class="table_title table_align_5">
